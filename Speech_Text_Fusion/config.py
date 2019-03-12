gradient clipping bug fixed

Speech_Text_Fusion/config.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 ## DEVICE: CPU / GPU
 # use GPU if available
 ##############################
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 # MAX_LEN of GloVe/ averaged COVAREP
 MAX_LEN = 50

Speech_Text_Fusion/utils/model_train.py
@@ -141,7 +141,7 @@ def train_audio_rnn(_epoch, clip, dataloader,
         # backward pass: compute gradient wrt model parameters
         loss.backward()
         # clip gradients
-        _ = torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        _ = torch.nn.utils.clip_grad_value_(model.parameters(), clip)
         # update weights
         optimizer.step()
         running_loss += loss.item()