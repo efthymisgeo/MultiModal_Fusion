0114749eaec6c120d96e31a565d3043ffadce6c2
efthymisgeo <efthymis.g.georgiou@gmail.com>
2019-03-15 15:17:18 +0200

    text/audio ft

--- Speech_Text_Fusion/main.py
+++ Speech_Text_Fusion/main.py
@@ -63,7 +63,7 @@ input_size = 300 # glove size
 hidden_size = 64 # hidden state size
 num_layers = 1 # how many stacked rnn's
 bidirectional = True
-dropout = [0.3, 0.5]
+dropout = 0.0
 architecture = 'GRU'
 attention_size = hidden_size
 batch_first = True
@@ -92,7 +92,7 @@ architecture = 'GRU'
 attention_size = hidden_size
 batch_first = True
 attn_layers = 1
-attn_dropout = 0.1
+attn_dropout = 0.05
 attn_nonlinearity = 'tanh'
 
 clip = 200.0
@@ -108,7 +108,7 @@ audio_hyperparameters = [input_size, hidden_size,
 # Training Text RNN Models
 ########################################
 drop = [0.0, 0.1, 0.5]
-EPOCHS_ = [75, 75, 75] 
+EPOCHS_ = [120, 120, 120] 
 lr_t = 0.00001
 for i,drop_i in enumerate(drop):
     #lr_t = 0.00001
